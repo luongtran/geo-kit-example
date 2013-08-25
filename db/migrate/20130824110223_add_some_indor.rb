4d7b3834891bfba754352ee101808674e603cb4f
LuanHt <htluan2605@gmail.com>
2013-08-25 21:28:12 +0700

    re sql

--- db/migrate/20130824110223_add_some_indor.rb
+++ db/migrate/20130824110223_add_some_indor.rb
@@ -2,8 +2,8 @@ class AddSomeIndor < ActiveRecord::Migration
   def up
     add_column :locations, :address, :string
     add_column :locations, :zip_code, :string
-    add_column :locations, :lat, :string
-    add_column :locations, :lng, :string
+    add_column :locations, :lat, :float
+    add_column :locations, :lng, :float
   end
 
   def down
--- db/migrate/20130824115043_change_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumn < ActiveRecord::Migration
-  def up
-    change_column :locations, :lat, :float
-    change_column :locations, :lng, :float
-  end
-
-  def down
-  end
-end
